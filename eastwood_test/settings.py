66d8be1bbf8638662b95c3dacfe6a7611f2cfbe1
Alex Yakovlev <zarmoose@ya.ru>
2018-06-05 14:19:09 +0300

    Описаны модели, создана БД (пока SQLite) и подключена админка.

--- eastwood_test/settings.py
+++ eastwood_test/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employees',
+    'bootstrap3',
     'phone_field',
+    'employees',
 ]
 
 MIDDLEWARE = [
--- employees/models.py
+++ employees/models.py
@@ -1,18 +1,37 @@
 from django.db import models
+from django.db.models import SET_NULL
 from phone_field import PhoneField
 
 
 # Create your models here.
+class Department(models.Model):
+    """Отдел компании"""
+
+    class Meta:
+        db_table = "department"
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Employee(models.Model):
+    """Сотрудник компании"""
+
     class Meta:
         db_table = "employee"
 
-    first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    birthday = models.DateField()
-    email = models.EmailField()
-    phone = PhoneField(blank=True)
-    begin_work = models.DateField()
-    end_work = models.DateField()
-    position = models.CharField(max_length=200)
+    first_name = models.CharField('Имя', max_length=50)
+    middle_name = models.CharField('Отчество', max_length=50)
+    last_name = models.CharField('Фамилия', max_length=50)
+    birthday = models.DateField('Дата рождения')
+    email = models.EmailField('e-mail')
+    phone = PhoneField('Телефон', blank=True)
+    begin_work = models.DateField('Начало работы')
+    end_work = models.DateField('Окончание работы', blank=True, null=True)
+    position = models.CharField('Должность', max_length=200)
+    department = models.ForeignKey(Department, on_delete=SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return ' '.join((self.last_name, self.first_name, self.middle_name, ))
