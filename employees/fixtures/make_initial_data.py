0b52680d68e37543adc05f54fd4febea03c110e5
Alex Yakovlev <zarmoose@ya.ru>
2018-06-08 07:39:06 +0300

    initial_data.json

--- employees/fixtures/make_initial_data.py
+++ employees/fixtures/make_initial_data.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python
 
-import json, random
+import json
+import random
 
 json_obj = []
 pk = 0
+positions = ["Должность №1", "Должность №2", "Должность №3", "Должность №4", "Должность №5"]
+departments = {1: "Бухгалтерия", 2: "Маркетинг", 3: "Разработка", 4: "Производство", 5: "Логитика"}
+
+for key in departments.keys():
+    json_item = {
+        "model": "employees.department",
+        "pk": key,
+        "fields": {
+            "name": departments.get(key)
+        }
+    }
+    json_obj.append(json_item)
 
 in_file = open("peeps", "r")
-
 for line in in_file:
     lmf = line.split()
     pk += 1
     birthday = '-'.join(['{:4d}'.format(random.randint(1950, 2000)), '{:02d}'.format(random.randint(1, 12)), '{:02d}'.format(random.randint(1, 28))])
+    begin_work = '-'.join(['{:4d}'.format(random.randint(2000, 2018)), '{:02d}'.format(random.randint(1, 12)), '{:02d}'.format(random.randint(1, 28))])
+    if random.choice([True, False]) == True:
+        end_work = '-'.join(['{:4d}'.format(random.randint(2000, 2018)), '{:02d}'.format(random.randint(1, 12)), '{:02d}'.format(random.randint(1, 28))])
+    else:
+        end_work = None
+    position = random.choice(positions)
+    department = random.choice(list(departments.keys()))
     json_item = {
         "model": "employees.employee",
         "pk": pk,
@@ -19,20 +38,17 @@ for line in in_file:
             "middle_name": lmf[2],
             "last_name": lmf[0],
             "birthday": birthday,
-            "email": "sema@sema.ru",
-            "phone": "+79998887766",
-            "begin_work": "2018-06-07",
-            "end_work": None,
-            "position": "\u0413\u043b\u0430\u0432\u0431\u0443\u0445",
-            "department": 6
+            "email": "writeme@letter.ru",
+            "phone": "+76665554433",
+            "begin_work": begin_work,
+            "end_work": end_work,
+            "position": position,
+            "department": department
         }
     }
     json_obj.append(json_item)
-
 in_file.close()
 
 out_file = open("__initial_data.json", "w")
-
 json.dump(json_obj, out_file, indent=4)
-
 out_file.close()
